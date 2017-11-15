7c02d5f3dbe6c7e9fac0b88bc86f8a51950806ac
gaoshang1999 <gaoshang1999@163.com>
2017-11-15 12:02:06 -0600

    finish sh files

--- BD/src/part3/part3.sh
+++ BD/src/part3/part3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-hadoop fs -mkdir /user/cloudera/part3  /user/cloudera/project/part3/input
+hadoop fs -rm -r /user/cloudera/part3
+hadoop fs -mkdir /user/cloudera/part3  /user/cloudera/part3/input
 echo "B12 C31 D76 A12 B76 B12 D76 C31 A10 B12 D76" > input
 echo "C31 D76 B12 A12 C31 D76 B12 A12 D76 A12 D76" >> input
 hadoop fs -put input /user/cloudera/part3/input
-hadoop fs -rm -r /user/cloudera/part3/output/*
-hadoop fs -rmdir -r /user/cloudera/part3/output
-hadoop jar wc.jar StripesRelativeFrequency  /user/cloudera/part3/input /user/cloudera/part3/output
+hadoop fs -rm -r /user/cloudera/part3/output
+hadoop jar wc.jar part3.StripesRelativeFrequency  /user/cloudera/part3/input /user/cloudera/part3/output
--- BD/src/part4/part4.sh
+++ BD/src/part4/part4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-hadoop fs -mkdir /user/cloudera/part4  /user/cloudera/project/part4/input
+hadoop fs -rm -r /user/cloudera/part4
+hadoop fs -mkdir /user/cloudera/part4  /user/cloudera/part4/input
 echo "B12 C31 D76 A12 B76 B12 D76 C31 A10 B12 D76" > input
 echo "C31 D76 B12 A12 C31 D76 B12 A12 D76 A12 D76" >> input
 hadoop fs -put input /user/cloudera/part4/input
-hadoop fs -rm -r /user/cloudera/part4/output/*
-hadoop fs -rmdir -r /user/cloudera/part4/output
-hadoop jar wc.jar HybridRelativeFrequency  /user/cloudera/part4/input /user/cloudera/part4/output
+hadoop fs -rm -r /user/cloudera/part4/output
+hadoop jar wc.jar part4.HybridRelativeFrequency  /user/cloudera/part4/input /user/cloudera/part4/output
